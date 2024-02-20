8c607efbf7df8ae8291018b9dadc2bb4f8001257
Akaninyene Archibong <godwin.noah@bbnplace.com>
2024-02-20 03:42:55 +0100

    Redified template columns for email and sms template tables

--- database/migrations/2024_02_01_083809_create_email_templates_table.php
+++ database/migrations/2024_02_01_083809_create_email_templates_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('email_templates', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->text('template')->index();
+            $table->text('template')->fulltext();
             $table->timestamps();
         });
     }
