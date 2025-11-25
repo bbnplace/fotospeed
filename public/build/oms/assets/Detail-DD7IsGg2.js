f173e4c82e5869063458ebad419b75e2be207c0e
Akaninyene Archibong <akanarchibong@Akaninyenes-MacBook-Pro.local>
2025-11-25 17:02:02 +0100

    Fixed routing bugs

---
+++ public/build/oms/assets/Detail-DD7IsGg2.js
@@ -0,0 +1,1 @@
+import{c as t,o as a,a as e,u as r,Z as o,F as m}from"./app-Bc2pWFvm.js";import{_ as s}from"./BackendLayout-BNQK4e-j.js";import"./indigo-CLkHYujY.js";import"./ApplicationLogo-D3i6zDr-.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";const d={__name:"Detail",setup(c){return(n,_)=>(a(),t(m,null,[e(r(o),{title:""}),e(s)],64))}};export{d as default};
