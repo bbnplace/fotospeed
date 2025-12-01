287e7300bcb0b3a710d7b24fb139d93de502789b
Akaninyene Archibong <akanarchibong@Akaninyenes-MacBook-Pro.local>
2025-12-01 06:42:44 +0100

    Applied new build entry javascript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,7 +39,7 @@
         <script src="{{ env('APP_URL') }}/assets/js/gsap/gsap-split-text.js"></script>
         <script src="{{ env('APP_URL') }}/build/showroom/assets/app-BzPtnHzn.js" type="module"></script>
     @else
-        <script src="{{ env('APP_URL') }}/build/oms/assets/app-DYjKnJN9.js" type="module"></script>
+        <script src="{{ env('APP_URL') }}/build/oms/assets/app-CN3v_eNs.js" type="module"></script>
     @endif
     
 </html>
